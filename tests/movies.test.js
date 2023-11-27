9136cd0ef277ad4a0ab4f2724d3fa32ab777263a
Farid-Zian <130969864+Farid-Zian@users.noreply.github.com>
2023-11-27 15:20:41 +0100

    frits commit

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -5,7 +5,7 @@ describe("GET /api/movies", () => {
 	it("should return all movies", async () => {
 		const response = await request(app).get("/api/movies");
 
-		expect(response.headers).toHaveProperty("content-type", /json/);
+		expect(response.headers["content-type"]).toMatch(/json/);
 		expect(response.status).toEqual(200);
 	});
 });
@@ -14,7 +14,7 @@ describe("GET /api/movies/:id", () => {
 	it("should return a movie by ID", async () => {
 		const response = await request(app).get("/api/movies/1");
 
-		expect(response.headers).toHaveProperty("content-type", /json/);
+		expect(response.headers["content-type"]).toMatch(/json/);
 		expect(response.status).toEqual(200);
 	});
 });
